64415c380548f460c9871bb58d73c1d3621ad29a
Thomas Paul CJ <118868993+ThomasPaulCJ@users.noreply.github.com>
2023-12-20 12:13:24 +0530

    Update Stokenizer.java

--- Stokenizer.java
+++ Stokenizer.java
@@ -4,6 +4,9 @@ public class Stokenizer {
 public static void main(String args[])
     {
     Scanner scan=new Scanner(System.in);
+    System.out.println("enter name of file to be created");
+    String name=scan.nextLine()+"txt";
+    FileWriter fw =new FileWriter(name);
     System.out.println("Enter the intergers:");
     String str=scan.nextLine();
     scan.close();
@@ -14,6 +17,7 @@ public static void main(String args[])
     int n=0;
     n=Integer.parseInt(s.nextToken());
     System.out.println("Ingegers are:"+n);
+    fw.write(n);
     sum=sum+n;
     }
     System.out.println("SUM OF INTEGERS ARE:"+sum);
